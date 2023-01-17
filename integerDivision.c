1e5c10187df2377d93dc74baceec635b78ee3d63
roshawn wright <89256902+Rwright7@users.noreply.github.com>
2023-01-16 23:24:01 -0500

    Create integerDivision.c

---
+++ integerDivision.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//write a function to perform integer division without using either the "/" or " * " operator. find a fast way to do it.
+
+void divide(int* dividend, int* divisor, int* quotient, int* remainder);
+
+int main(void)
+{
+       int dividend = 10, divisor = 3, quotient, remainder;
+       
+       divide(&dividend, &divisor, &quotient, &remainder);
+       
+       printf("The Answer is %d\n", quotient);
+
+       return 0;
+}
+
+void divide(int* dividend, int* divisor, int* quotient, int* remainder)
+{
+       *quotient = 0;
+       *remainder = 0;
+       int sign = 1;
+  
+       if(*divisor == 0)
+       {
+            *quotient = -1;
+            *remainder = 0;
+            return;
+       }
+  
+       if(*dividend < 0)
+       {
+            sign = -sign;
+            *dividend = -(*dividend);
+       }
+
+}
